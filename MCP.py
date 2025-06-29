95d2832cc6ba9c23180e1865974e728d7023dd9b
nekorobi0 <hogefuga@ex.com>
2025-06-29 20:11:21 +0900

    おけ

--- MCP.py
+++ MCP.py
@@ -1,19 +1,18 @@
-from fastmcp import FastMCP,Context,exceptions
+import fastapi
+import uvicorn
 from read_DHT11 import DHT11Sensor
 import board
 # Create a server instance
-mcp = FastMCP(name="Temperature and Humidity Sensor")
+app = fastapi.FastAPI(summary="Temperature and Humidity Sensor", description="Provides temperature and humidity data")
 dht_sensor = DHT11Sensor(board.D23)
-@mcp.resource(
-    uri="sensor://temperature_and_humidity",
+@app.post(
+    path="/temperature_and_humidity",
     name="Temperature and Humidity Sensor",
     description="Provides temperature and humidity data"
 )
-def get_temp_and_humidity(ctx: Context) -> dict:
-    try:
-        tempraure, humidity = dht_sensor.get_data()
-        return {"temperature_celsius": tempraure, "humidity_percent": humidity}
-    except:
-        raise exceptions.ResourceError("Failed to read temperature and humidity data")
+def get_temp_and_humidity() -> dict:
+    tempraure, humidity = dht_sensor.get_data()
+    return {"temperature_celsius": tempraure, "humidity_percent": humidity}
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http",host="192.168.0.236")
+    
+    uvicorn.run(host="192.168.0.236",port=8000, app=app)
